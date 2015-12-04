0535ee3d9a98689064bcc8bc392b8b4d73ff8c58
Fabien Fivaz <fabien.fivaz@unine.ch>
2015-12-04 16:57:45 +0100

    It's beginning to look gooood!

--- js/functions.js
+++ js/functions.js
@@ -1,5 +1,5 @@
 /**
- * Sticky header example found there: http://jsfiddle.net/livibetter/HV9HM/
+ * Sticky header example inspiration from: http://jsfiddle.net/livibetter/HV9HM/
  */
 function sticky_relocate() {
     var window_top = $(window).scrollTop();
@@ -19,12 +19,12 @@ $(function () {
 });
 
 /**
- * offset tip found at http://denverdias.com/2014/11/19/bootstrap-scrollspy-tips/
+ * offset tip found inspiration from http://denverdias.com/2014/11/19/bootstrap-scrollspy-tips/
  */
 $('body').scrollspy({selector: '.scrollclass', offset: 65});
 
 /**
- * Smooth scrolling to anchor. Found here : http://stackoverflow.com/questions/7717527/jquery-smooth-scrolling-when-clicking-an-anchor-link
+ * Smooth scrolling to anchor. inspiration from: http://stackoverflow.com/questions/7717527/jquery-smooth-scrolling-when-clicking-an-anchor-link
  */
 $( document ).ready(function() {
     $('a').click(function () {
